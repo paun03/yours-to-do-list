94ee04a143de9902f2aefbe551d93f9484898206
paun03 <paunovicm2003@gmail.com>
2024-01-01 07:38:34 +0100

    commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,32 +6,55 @@ let insertInput = document.querySelector("#insertInput");
 let btnSendTask = document.querySelector("#sendTask");
 let btnRestartInput = document.querySelector("#restartInput");
 
-console.log(btnSendTask);
-
 let taskContainerPending = document.querySelector(".taskContainerPending");
 
+// FUNCTIONS
+
 let sendTaskTo = () => {
-    let taskContainer = document.createElement("div");
-    taskContainer.classList.add("taskContainer");
-    let task = document.createElement("div");
-    task.classList.add("task");
-    let parSend = document.createElement("p");
-    parSend.textContent = `${insertInput.value}`;
-    let btnFinishTask = document.createElement("button");
-    let btnRemoveTask = document.createElement("button");
-    btnFinishTask.textContent = "✔";
-    btnRemoveTask.textContent = "✘";
-    btnFinishTask.setAttribute("id", "finishTask");
-    btnRemoveTask.setAttribute("id", "removeTask");
+    if (insertInput.value === "") {
+        alert("Empty!");
+    } else {
+        let taskContainer = document.createElement("div");
+        taskContainer.classList.add("taskContainer");
+        let task = document.createElement("div");
+        task.classList.add("task");
+        let parSend = document.createElement("p");
+        parSend.textContent = `${insertInput.value}`;
+        let btnFinishTask = document.createElement("button");
+        let btnRemoveTask = document.createElement("button");
+        btnFinishTask.textContent = "✔";
+        btnRemoveTask.textContent = "✘";
+        btnFinishTask.setAttribute("id", "finishTask");
+        btnRemoveTask.setAttribute("id", "removeTask");
 
-    task.appendChild(parSend);
-    taskContainer.appendChild(task);
-    taskContainer.appendChild(btnFinishTask);
-    taskContainer.appendChild(btnRemoveTask);
+        task.appendChild(parSend);
+        taskContainer.appendChild(task);
+        taskContainer.appendChild(btnFinishTask);
+        taskContainer.appendChild(btnRemoveTask);
 
-    taskContainerPending.appendChild(taskContainer);
-}
+        taskContainerPending.appendChild(taskContainer);
+    }
+};
+
+let removeTaskFrom = () => {
+    insertInput.value = "";
+};
 
 btnSendTask.addEventListener("click", () => {
     sendTaskTo();
+});
+
+btnRestartInput.addEventListener("click", () => {
+    removeTaskFrom();
+});
+
+taskContainerPending.addEventListener("click", (event) => {
+    const clickedElement = event.target;
+
+    if (clickedElement.tagName === 'BUTTON' && clickedElement.id === 'removeTask') {
+        const taskContainer = clickedElement.closest('.taskContainer');
+        if (taskContainer) {
+            taskContainer.remove();
+        }
+    }
 });
